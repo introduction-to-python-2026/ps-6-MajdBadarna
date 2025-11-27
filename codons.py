47c90c118ef394cb8f82b40fd7da5eff156ab54c
MajdBadarna <q7ztqqwrn2@privaterelay.appleid.com>
2025-11-27 14:07:02 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
 
+    codon_dict = {}  # מילון ריק
+    with open(file_path, 'r') as file:  # פותחים את הקובץ
+        rows = file.readlines()          # קוראים את כל השורות
 
+    for row in rows:
+        cells = row.strip().split('\t')  # מסירים רווחים ו-\n ומפרקים לפי טאבים
+        codon = cells[0]                 # הקודון הוא התא הראשון
+        amino_acid = cells[2]            # הקיצור של חומצת האמינו הוא התא השלישי
+        codon_dict[codon] = amino_acid   # מוסיפים למילון
+
+    return codon_dict
